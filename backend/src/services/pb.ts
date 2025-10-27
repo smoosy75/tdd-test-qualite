a1c7fdbef3942809cb9ea656544ebf135f368220
Mustapha Said <mustaphasaid@MacBookAir.lan>
2025-10-27 15:34:03 +0100

    add docker

---
+++ backend/src/services/pb.ts
@@ -0,0 +1,13 @@
+import PocketBase from "pocketbase";
+
+const pb = new PocketBase(
+  process.env.POCKETBASE_URL || "http://127.0.0.1:8090"
+);
+
+// juste un exemple il faut Crée ton compte admin sur pocketbase (login + mot de passe).
+export async function getAllPosts() {
+  const records = await pb.collection("posts").getFullList();
+  return records;
+}
+
+export default pb;
