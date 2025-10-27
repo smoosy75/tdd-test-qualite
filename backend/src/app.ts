86f738f35112876e7d661085a5fe44ff5ae3d9d4
Mustapha Said <mustaphasaid@MacBookAir.lan>
2025-10-27 14:55:14 +0100

    congig jest for backend and route for react

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,11 +2,12 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 app.get("/hello", (_, res) => {
-  res.json({ status: "ok" });
+  res.status(200).json({ status: "ok" });
 });
 
 export default app;
--- backend/src/tests/hello.test.ts
+++ backend/src/tests/hello.test.ts
@@ -1,9 +1,9 @@
 import request from "supertest";
 import app from "../app";
 
-describe("Health check", () => {
+describe("hello check", () => {
   it("should return status ok", async () => {
-    const res = await request(app).get("/health");
+    const res = await request(app).get("/hello");
     expect(res.status).toBe(200);
     expect(res.body).toEqual({ status: "ok" });
   });
