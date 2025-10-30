e970c385c4877bc9cdb4124ea7d0ecd7da0fb84c
Vladislav <57127875+superrnovae@users.noreply.github.com>
2025-10-30 11:22:03 +0100

    backend: Corriger l'instantation des controlleurs

--- backend/src/routes/posts.routes.ts
+++ backend/src/routes/posts.routes.ts
@@ -3,20 +3,28 @@ import { PostsController } from "../controllers/posts.controller";
 import PostService from "../services/postService";
 import { CommentService } from "../services/commentService";
 import pool from "../services/db";
+import { UserService } from "../services/userService";
+import pb from "../services/pb";
 
 const router = express.Router();
-const comment = new CommentService(pool);
-const postsService = new PostService(pool, comment)
-const { fetchPosts, fetchPostById } = new PostsController(postsService);
+const commentsService = new CommentService(pool);
+const postsService = new PostService(pool, commentsService)
+const userService = new UserService(pb)
 
 /**
  * GET /api/posts
  */
-router.get("/", fetchPosts);
+router.get("/", async (req, res) => {
+  const controller = new PostsController(postsService, userService);
+  return controller.fetchPosts(req, res);
+});
 
 /**
  * GET /api/posts/:id
  */
-router.get("/:id", fetchPostById);
+router.get("/:id", async (req, res) => {
+  const controller = new PostsController(postsService, userService);
+  return controller.fetchPostById(req, res);    
+});
 
 export default router;
