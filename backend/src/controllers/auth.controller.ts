e8fe8dd18c2f7d90301213bb70828245aee2cffd
Mircea Bardan <mircea.c.bardan@gmail.com>
2025-10-31 11:28:42 +0100

    add vulnerabilities (raw token and eval())

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -21,7 +21,6 @@ export async function signup(req: Request, res: Response): Promise<Response> {
     });
 
     console.log('✅ /auth/signup SUCCESS', user);
-    eval("console.log('vulnerable')");
 
     return res.status(201).json({ user });
   } catch (error: any) {
